ef27c1f87a575ccb8fee6f352a30d4b60f410721
chebupa <aristarhsakulin@gmail.com>
2026-03-21 09:50:51 +0300

    initial setup

---
+++ rtk-pass/rtk-pass/rtk_passApp.swift
@@ -0,0 +1,17 @@
+//
+//  rtk_passApp.swift
+//  rtk-pass
+//
+//  Created by aristarh on 21.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct rtk_passApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
